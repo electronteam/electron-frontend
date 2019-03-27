bb37dc4a7ad76aaf0a1c7a2c78abef7e91a19283
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-03-27 23:52:37 +0200

    Added a health check ajax call

--- src/App.js
+++ src/App.js
@@ -3,21 +3,41 @@ import {BrowserRouter, Link, Route} from 'react-router-dom';
 import './App.css';
 import {Home} from "./Home";
 import {Test} from "./Test";
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
+
+    state = {message : ''};
+
+    healthCheckFromBackend () {
+        fetch('http://localhost:8080/healthCheck')
+        .then(response => response.text())
+        .then(message => {
+            this.setState({message: message});
+        });
+    }
+
     render()
     {
         return (
-                <BrowserRouter>
-                    <nav>
-                        <Link to="/" className="link">Home</Link>
-                        <Link to="/test"  className="link">Test</Link>
-                    </nav>
+                <MuiThemeProvider>
+                    <BrowserRouter>
+                        <nav>
+                            <Link to="/" className="link">Home</Link>
+                            <Link to="/test"  className="link">Test</Link>
+                        </nav>
 
-                    <Route path="/" exact={true} component={Home}/>
-                    <Route path="/test" component={Test}/>
-                </BrowserRouter>
-        );
+                        <Route path="/" exact={true} component={Home}/>
+                        <Route path="/test" component={Test}/>
+
+
+                        <RaisedButton label="Health check from backend" onClick={this.healthCheckFromBackend.bind(this)} style="color:green"/>
+
+                        <p>{this.state.message}</p>
+                    </BrowserRouter>
+                </MuiThemeProvider>
+    );
     }
 }
 
