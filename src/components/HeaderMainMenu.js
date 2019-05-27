92e4c9c8dbd3434681cffae08a2ff17482b83e76
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-05-27 17:32:33 +0300

    More styling

--- src/components/HeaderMainMenu.js
+++ src/components/HeaderMainMenu.js
@@ -2,30 +2,34 @@ import React from 'react';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
 import {properties} from '../properties.js';
 
-export function HeaderMainMenu({ token })
+export function HeaderMainMenu()
 {
     return (
             <BrowserRouter>
-                <header>
-                    <Link to="/" className="logo">
-                        <img src="electron-logo.png" alt="LOGO" className="logo"/>
-                    </Link>
-                    <ul className="main-nav">
-                        {properties.header.links.map((link, index) => {
-                            return (
-                                    <li key={index}>
-                                        <Link to={link.path} className="link">
-                                            {link.displayText}
-                                        </Link>
-                                    </li>
-                            )
-                        })}
-                    </ul>
+                <header className="site-navbar">
+                    <div className="site-navbar-top">
+                        <Link to="/" className="logo">
+                            <img src="electron-logo.png" alt="LOGO" className="logo"/>
+                        </Link>
+                    </div>
+                    <nav className="site-navigation text-right text-md-center" role="navigation">
+                        <ul className="main-nav">
+                            {properties.header.links.map((link, index) => {
+                                return (
+                                        <li key={index}>
+                                            <Link to={link.path} className="link">
+                                                {link.displayText}
+                                            </Link>
+                                        </li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
                 </header>
 
                 {properties.header.links.map((link, index) => {
                     return (
-                            <Route path={link.path} exact={true} component={link.component} key={index} />
+                            <Route path={link.path} exact={true} component={link.component} key={index}/>
                     )
                 })}
             </BrowserRouter>
