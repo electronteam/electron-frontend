6044007811f3aae7e5d5e1970db675cc7db6b995
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-05-27 14:53:05 +0300

    Added some test images and resolving this based on product code

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -41,7 +41,9 @@ class ProductList extends Component {
                                 <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up" key={index}>
                                     <div className="block-4 text-center border">
                                         <figure className="block-4-image">
-                                            <a href="shop-single.html"><img src="http://placehold.it/700x400" alt="Image placeholder" className="img-fluid"/></a>
+                                            <a href="shop-single.html">
+                                                <img src={"img/product_" + product.code + ".png"} alt="Image placeholder" className="img-fluid"/>
+                                            </a>
                                         </figure>
                                         <div className="block-4-text p-4">
                                             <h3><a href="shop-single.html">{product.name}</a></h3>
