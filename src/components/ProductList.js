3e69c02ed6f5581bb2f38ac2283e2a18be19c4b3
Radu Baroncea <rbaroncea@tacitknowledge.com>
2020-01-31 14:47:35 +0200

    Display a suggestive text in case no products have been fetched in ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -37,36 +37,38 @@ class ProductList extends Component {
     {
         return (
                 <div className="row">
-                    <div className="col-lg-12">
-                        <div className="row mb-5">
+                    {this.state.products.length > 0 ?
+                        <div className="col-lg-12">
+                            <div className="row mb-5">
 
-                            {this.state.products.map((product, index) => {
-                                return (
-                                        <div className="col-sm-6 col-lg-3 mb-3" data-aos="fade-up" key={index}>
-                                            <div className="block-4 text-center">
-                                                <div className="product">
-                                                    <div className="product-img">
-                                                        <figure className="block-4-image">
-                                                            <Link to={{pathname: properties.product.path + product.code}}>
-                                                                <img src={"/img/product_" + product.code + ".png"} alt=""/>
-                                                            </Link>
-                                                        </figure>
-                                                    </div>
-                                                    <div className="product-body">
-                                                        <div className="block-4-text p-4">
-                                                            <Link to={{pathname: properties.product.path + product.code}}>
-                                                                <h1 className="product-name">{product.name}</h1>
-                                                            </Link>
-                                                            <h4 className="product-price">{product.price} lei</h4>
+                                {this.state.products.map((product, index) => {
+                                    return (
+                                            <div className="col-sm-6 col-lg-3 mb-3" data-aos="fade-up" key={index}>
+                                                <div className="block-4 text-center">
+                                                    <div className="product">
+                                                        <div className="product-img">
+                                                            <figure className="block-4-image">
+                                                                <Link to={{pathname: properties.product.path + product.code}}>
+                                                                    <img src={"/img/product_" + product.code + ".png"} alt=""/>
+                                                                </Link>
+                                                            </figure>
+                                                        </div>
+                                                        <div className="product-body">
+                                                            <div className="block-4-text p-4">
+                                                                <Link to={{pathname: properties.product.path + product.code}}>
+                                                                    <h1 className="product-name">{product.name}</h1>
+                                                                </Link>
+                                                                <h4 className="product-price">{product.price} lei</h4>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                )
-                            })}
+                                    )
+                                })}
+                            </div>
                         </div>
-                    </div>
+                    : <h1>No Products</h1>}
                     {/*col-lg-12*/}
                 </div>
         );
